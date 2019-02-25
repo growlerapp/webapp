b59f7c69ca5b0b804bb1c75a0b7c15786eaba9e9
raulghm <raulghm@gmail.com>
2019-02-24 22:42:21 -0300

    fix(): agrega nuevo prop button

--- src/components/v-Button.vue
+++ src/components/v-Button.vue
@@ -1,12 +1,26 @@
 <template>
-  <button class="v-Button">
+  <div class="v-Button" :class="[_type]">
     <slot></slot>
-  </button>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'v-Button'
+  name: 'v-Button',
+
+  props: {
+    type: {
+      type: String
+    }
+  },
+
+  computed: {
+    _type () {
+      return {
+        'v-Button--alt': this.type === 'alt'
+      }
+    }
+  }
 }
 </script>
 
@@ -17,11 +31,12 @@ export default {
   letter-spacing: 1px;
   font-weight: 500;
   background-color: var(--color-red);
+  color: var(--color-white);
   display: inline-block;
   padding: 10px 20px;
   border-radius: 20px;
-  color: var(--color-white);
   cursor: pointer;
+  line-height: 1.1;
 }
 
 .v-Button:hover {
@@ -31,5 +46,19 @@ export default {
 .v-Button:active,
 .v-Button:focus {
   background-color: color(var(--color-red) b(10%));
+}
+
+.v-Button--alt {
+  background-color: var(--color-white);
+  color: var(--color-red);
+}
+
+.v-Button--alt:hover {
+  background-color: color(var(--color-red) l(90%));
+}
+
+.v-Button--alt:active,
+.v-Button--alt:focus {
+  background-color: color(var(--color-white) b(10%));
 }
 </style>
