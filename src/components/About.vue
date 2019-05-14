4e2b360aeb9204bf54e978239d3625a9cf3899ca
Juan Eduardo Keldor <50382219+juankeldor@users.noreply.github.com>
2019-05-14 15:11:36 -0400

    chore(text): added texto to this page

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,6 +1,9 @@
 <template>
   <div>
-		<h1>About</h1>
+		<h1>Sobre Growler App</h1>
+    <p>Esta aplicación busca acercar a los amantes de la buena cerveza artesanal con productores que permiten el llenado de <i>growlers</i> con sus variedades de cerveza.</p>
+    <p>Está desarrollada bajo código <a href="https://github.com/growlerapp/" target="_blank">Open-Source</a> disponible para mejoras y aprendizaje.</p>
+    <p>Hecho con ❤️ y 🍺 por quienes <a href="https://github.com/orgs/growlerapp/people" target="_blank">voluntariamente contribuyen</a>.</p>
 	</div>
 </template>
 
