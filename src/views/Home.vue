68e142e9e121e6bbe7717c9ef7204e5fcc10ff8a
Raul Hernandez <raulghm@gmail.com>
2019-10-28 11:14:42 -0300

    fix: mejoras validaciones y mensajes de error

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="Home pull-to-refresh-ios">
-    <loading v-if="!findByProximity"/>
+    <loading v-if="!isfetchError && loading"/>
 
     <div class="pull-to-refresh-ios__spinner">
       <div
@@ -12,14 +12,14 @@
 
     <div class="pull-to-refresh-ios__main">
       <transition-group
-        v-if="findByProximity"
+        v-if="stores && isGeoError === null"
         name="fade"
         tag="div"
         class="Home-body"
         appear
       >
         <StoreItem
-          v-for="store in findByProximity"
+          v-for="store in stores"
           :key="store._id"
           :id="store._id"
           :name="store.name"
@@ -30,8 +30,8 @@
         />
       </transition-group>
 
-      <div class="Home-empty" v-if="findByProximity && !findByProximity.length">
-        <img class="Home-empty-icon" src="/img/img-growler-empty.svg" alt="growler">
+      <div class="Home-error" v-if="stores && !stores.length">
+        <img class="Home-error-icon" src="/img/img-growler-empty.svg" alt="growler">
         <h2>
           No encontramos bares cercanos
         </h2>
@@ -41,10 +41,46 @@
           <span v-show="!isTouchDevice()">recargar</span>
           para actualizar
         </p>
-        <img class="Home-empty-sep" src="/img/img-separator.svg" alt="sep">
+        <img class="Home-error-sep" src="/img/img-separator.svg" alt="sep">
         <small>
           Próximamente podrás agregar nuevos bares que sean de tu interés
         </small>
+      </div>
+
+      <div class="Home-error" v-if="isfetchError">
+        <img class="Home-error-icon" src="/img/img-growler-empty.svg" alt="growler">
+        <h2>
+          Ha ocurrido <br> un error al obtener información
+        </h2>
+        <p>
+          Prueba
+          <span v-show="isTouchDevice()">deslizar hacia abajo</span>
+          <span v-show="!isTouchDevice()">recargar</span>
+          para actualizar
+        </p>
+        <v-button @click.native="fetchData(true, true)">Reintentar</v-button>
+      </div>
+
+      <div class="Home-error" v-if="isGeoError !== null">
+        <img class="Home-error-icon" src="/img/img-growler-empty.svg" alt="growler">
+        <template v-if="isGeoError === 0">
+          <h2>Debes autorizar la geolocalización en tu dispositivo para continuar</h2>
+        </template>
+        <template v-else-if="isGeoError === 1">
+          <h2>No podemos detectar donde te encuentras</h2>
+        </template>
+        <template v-else>
+          <h2>Ha ocurrido un error tratando de obtener tu geolocalización</h2>
+        </template>
+        <p>
+          Prueba
+          <span v-show="isTouchDevice()">deslizar hacia abajo</span>
+          <span v-show="!isTouchDevice()">recargar</span>
+          para actualizar
+        </p>
+        <div class="Splash-continue">
+          <v-button @click.native="fetchData()">Reintentar</v-button>
+        </div>
       </div>
     </div>
   </div>
@@ -58,21 +94,30 @@ import 'mobile-pull-to-refresh/dist/styles/ios/style.css'
 import FIND_BY_PROXIMITY from '@/graphql/FindByProximity.gql'
 import StoreItem from '@/components/StoreItem'
 import Loading from '@/components/Loading'
+import vButton from '@/components/v-Button'
 
 export default {
   name: 'Home',
 
   components: {
     StoreItem,
-    Loading
+    Loading,
+    vButton
   },
 
   mounted () {
-    this.refresh()
+    this.fetchData()
+    this.pullToRefresh()
   },
 
+  data: () => ({
+    loading: true,
+    isfetchError: null,
+    isGeoError: null
+  }),
+
   apollo: {
-    findByProximity: {
+    stores: {
       query: FIND_BY_PROXIMITY,
       variables () {
         return {
@@ -81,27 +126,49 @@ export default {
           googleMapsKey: process.env.VUE_APP_GOOGLE_MAPS_KEY
         }
       },
+      $query: {
+        fetchPolicy: 'network-only'
+      },
       update ({ findByProximity }) {
-        return findByProximity.map(store => {
+        this.loading = false
+        return findByProximity && findByProximity.map(store => {
           const copy = Object.assign({}, store)
           copy.matrix = store.matrix.find(({ mode }) => mode === 'driving')
           return copy
         })
-      }
+      },
+      error () {
+        this.isfetchError = true
+      },
+      skip: true
     }
   },
 
   methods: {
-    refresh () {
+    pullToRefresh () {
       const context = this
       pullToRefresh({
         container: document.querySelector('.Home'),
         animates: ptrAnimatesIos,
         async refresh () {
-          context.$store.dispatch('userGeoData', { force: true })
-          await context.$apollo.queries.findByProximity.refetch()
+          await context.fetchData(false)
         }
       })
+    },
+
+    async fetchData (loading = true, reload = false) {
+      if (reload) window.location.reload(true)
+      try {
+        await this.$store.dispatch('userGeoData', { force: true })
+        this.isGeoError = null
+        this.isfetchError = null
+        this.loading = loading
+        this.$apollo.queries.stores.skip = false
+        await this.$apollo.queries.stores.refetch()
+      } catch (error) {
+        this.loading = false
+        this.isGeoError = error.code
+      }
     },
 
     isTouchDevice () {
@@ -159,36 +226,36 @@ export default {
   }
 }
 
-.Home-empty {
+.Home-error {
   text-align: center;
   padding-top: 4em;
   max-width: 280px;
   margin: 0 auto;
 }
-.Home-empty h2 {
+.Home-error h2 {
   text-transform: uppercase;
   line-height: 1.5;
 }
-.Home-empty p {
+.Home-error p {
   font-size: 1.2rem;
   font-family: var(--font-family-normal);
 }
-.Home-empty-icon {
+.Home-error-icon {
   max-width: 80px;
   display: inline-block;
   opacity: .4;
   transform: rotate(10deg);
 }
 @media (--lg-viewport) {
-  .Home-empty-icon {
+  .Home-error-icon {
     max-width: 120px;
   }
 }
-.Home-empty-sep {
+.Home-error-sep {
   display: inline-block;
   width: 120px;
 }
-.Home-empty small {
+.Home-error small {
   display: inline-block;
   margin-top: 1em;
   text-transform: uppercase;
